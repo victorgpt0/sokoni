5e579552b2f8704139c4af2c8d799af68e1c717d
Victor Gichuru <175977320+victorgpt0@users.noreply.github.com>
2025-09-28 14:05:23 +0300

    subnet and az fixes

--- .github/infra/staging/main.tf
+++ .github/infra/staging/main.tf
@@ -2,7 +2,7 @@ module "app" {
   source = "./module"    
     env = "staging"
 
-    availability_zones = [ "us-east-1c" ]
+    availability_zones = ["us-east-1c", "us-east-1d"]
     public_subnets = [ "10.0.1.0/24", "10.0.2.0/24" ]
     private_subnets = [ "10.0.101.0/24", "10.0.102.0/24" ]
 
