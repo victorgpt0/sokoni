bd5026aaa244fa72dd57f4ce0a66de2c15c92965
Victor Gichuru <175977320+victorgpt0@users.noreply.github.com>
2025-09-28 17:05:21 +0300

    add role and policy for ec2

--- .github/infra/staging/module/ecs.tf
+++ .github/infra/staging/module/ecs.tf
@@ -64,6 +64,32 @@ resource "aws_iam_policy_attachment" "ecs_task_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"  
 }
 
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "sokoni-${var.env}-ecs-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    terraform = "true"
+  }  
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  
+}
 resource "aws_ecs_task_definition" "app" {
   family                   = "sokoni-${var.env}-task"
   execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
