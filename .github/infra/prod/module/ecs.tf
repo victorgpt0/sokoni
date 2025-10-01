cd4d283278d60714078518546cbeef07a00cbee9
Victor Gichuru <175977320+victorgpt0@users.noreply.github.com>
2025-10-01 20:25:37 +0300

    pink pantheress banging

--- .github/infra/prod/module/ecs.tf
+++ .github/infra/prod/module/ecs.tf
@@ -100,7 +100,7 @@ resource "aws_ecs_task_definition" "migrations" {
       portMappings = [
         {
           containerPort = var.app_port
-          hostPort      = 0
+          hostPort      = var.app_port
           protocol      = "tcp"
         }
       ]
