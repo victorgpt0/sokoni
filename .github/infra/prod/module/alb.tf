efcb61a2e51644e8f28dcc7cead2185c53ca90f3
Victor Gichuru <175977320+victorgpt0@users.noreply.github.com>
2025-10-03 00:10:32 +0300

    ksi

--- .github/infra/prod/module/alb.tf
+++ .github/infra/prod/module/alb.tf
@@ -61,6 +61,9 @@ resource "aws_acm_certificate" "default" {
 }
 
 resource "null_resource" "dns_validation" {
+  triggers = {
+    validation_value = local.txt_value
+  }
   provisioner "local-exec" {
     command = <<EOT
       curl "https://www.duckdns.org/update?domains=${var.duckdns_domain}&token=${var.duckdns_token}&txt=${local.txt_value}&verbose=true"
