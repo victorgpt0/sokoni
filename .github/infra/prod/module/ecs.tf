0aedac6a488bfc0994621a7c873b0647d283080e
Victor Gichuru <175977320+victorgpt0@users.noreply.github.com>
2025-10-17 10:07:32 +0300

    instance tg

--- .github/infra/prod/module/ecs.tf
+++ .github/infra/prod/module/ecs.tf
@@ -431,7 +431,7 @@ resource "aws_ecs_service" "app" {
   launch_type     = "EC2"
 
   load_balancer {
-    target_group_arn = aws_alb_target_group.app_tg.arn
+    target_group_arn = aws_alb_target_group.app_instance_tg.arn
     container_name   = "app"
     container_port   = var.app_port
   }
