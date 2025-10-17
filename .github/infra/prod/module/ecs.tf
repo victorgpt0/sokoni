0d0e13fcb505da54509c84a5771b01cd0c2283ba
Victor Gichuru <175977320+victorgpt0@users.noreply.github.com>
2025-10-17 11:13:58 +0300

    rm public ips

--- .github/infra/prod/module/ecs.tf
+++ .github/infra/prod/module/ecs.tf
@@ -152,6 +152,7 @@ resource "aws_launch_template" "ecs" {
 
   network_interfaces {
     security_groups = [ aws_security_group.ecs.id ]
+    associate_public_ip_address = false
   }
 
   user_data = base64encode(<<-EOT
