0131b22cbf8c2841dbe131a12e6356ba739b5995
Victor Gichuru <175977320+victorgpt0@users.noreply.github.com>
2025-10-28 22:25:08 +0300

    add ssm policy to instance

--- .github/infra/prod/module/ecs.tf
+++ .github/infra/prod/module/ecs.tf
@@ -65,6 +65,11 @@ resource "aws_iam_role_policy_attachment" "ecs_instance_cloudwatch_role_policy" 
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_instance_ssm_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
   name = "sokoni-${var.env}-ecs-instance-profile"
   role = aws_iam_role.ecs_instance_role.name  
